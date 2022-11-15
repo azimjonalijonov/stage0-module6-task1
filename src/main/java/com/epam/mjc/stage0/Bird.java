d7fa44889d1dba9a9a80d7e3df09fb33953968d8
azimjonalijonov <115147667+azimjonalijonov@users.noreply.github.com>
2022-11-15 20:50:19 +0500

    Update Bird.java

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -5,7 +5,7 @@ package com.epam.mjc.stage0;
 public class Bird extends Animal {
 
     public Bird(String color, int numberOfPaws, boolean hasFur) {
-        super("blue", 2, true);
+        super("blue", 2, false);
     }
 
     public Bird() {
